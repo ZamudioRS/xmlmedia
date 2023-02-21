25bd8ba2289e5edb08b116a8780a12110789892f
ZamudioRS <eduardo.zamudio@redshelf.com>
2023-02-20 20:03:36 -0600

    add: server and client side titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,9 @@ export default function Home({product}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>SERVER SIDE</h1>
       <Card data={product}/>
+      <h1>CLIENT SIDE</h1>
       <CardClientSide/>
     </>
   )
